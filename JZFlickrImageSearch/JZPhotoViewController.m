3ffb97ee0df7c7ff43e40cb9c0301bce62527f4a
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-10-20 13:49:57 +0300

    add zoom and rotation (need to fix scroll after zooming)

--- JZFlickrImageSearch/JZPhotoViewController.m
+++ JZFlickrImageSearch/JZPhotoViewController.m
@@ -9,9 +9,10 @@
 #import "JZPhotoViewController.h"
 #import "UIImageView+AFNetworking.h"
 
-@interface JZPhotoViewController ()
+@interface JZPhotoViewController ()<UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 - (IBAction)clickCloseButton:(id)sender;
 
@@ -24,6 +25,26 @@
     
     [self.imageView setImageWithURL:self.photoURL
                    placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    
+    UIRotationGestureRecognizer *rotationGestureRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotationWithGestureRecognizer:)];
+    [self.imageView addGestureRecognizer:rotationGestureRecognizer];
+    
+    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchZoomWithGestureRecognizer:)];
+    [self.imageView addGestureRecognizer:pinchGestureRecognizer];
+    
+    self.scrollView.contentSize=self.imageView.frame.size;
+}
+
+-(void)handlePinchZoomWithGestureRecognizer:(UIPinchGestureRecognizer *)pinchGestureRecognizer {
+    self.imageView.transform = CGAffineTransformScale(self.imageView.transform, pinchGestureRecognizer.scale, pinchGestureRecognizer.scale);
+    pinchGestureRecognizer.scale = 1.0;
+    self.scrollView.contentSize = self.imageView.frame.size;
+    
+}
+-(void)handleRotationWithGestureRecognizer:(UIRotationGestureRecognizer *)rotationGestureRecognizer{
+    self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, rotationGestureRecognizer.rotation);
+    
+    rotationGestureRecognizer.rotation = 0.0;
 }
 
 - (IBAction)clickCloseButton:(id)sender {
